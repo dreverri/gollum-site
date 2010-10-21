ff65c18ac0cd0168718e92e3060c8c689a0f1a96
Daniel Reverri <reverri@gmail.com>
2010-10-20 23:45:50 -0700

    Incorporated newer versions of certain Gollum objects that contain functionality required by site generation

--- lib/gollum-site.rb
+++ lib/gollum-site.rb
@@ -2,3 +2,8 @@ require 'gollum'
 require 'liquid'
 require 'gollum-site/page'
 require 'gollum-site/site'
+
+# Site generation depends on methods not available in 1.0.1
+# Including more recent version of some objects
+require 'gollum-site/blob_entry'
+require 'gollum-site/wiki'

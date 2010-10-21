4123034ead47505f54b1d1bf181d24e84629c55f
Daniel Reverri <reverri@gmail.com>
2010-10-21 14:22:48 -0700

    Modifed find() to remove .html extension when looking for pages

--- lib/gollum-site/page.rb
+++ lib/gollum-site/page.rb
@@ -3,9 +3,19 @@ module Gollum
     # Add ".html" extension to page links
     def self.cname(name)
       cname = name.respond_to?(:gsub)      ?
-        name.gsub(%r{[ /<>]}, '-') :
+      name.gsub(%r{[ /<>]}, '-') :
         ''
       cname + '.html'
+    end
+
+    def find(cname, version)
+      name = cname[0..-6]
+      if commit = @wiki.repo.commit(version)
+        if page = find_page_in_tree(commit.tree, name)
+          page.version = commit
+          page
+        end
+      end
     end
   end
 end
